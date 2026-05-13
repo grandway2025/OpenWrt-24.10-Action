130e8950a5c4a7a2ba18a271f898b7c8b7571e9a
grandway2025 <zouchanggan@gmail.com>
2026-05-13 22:39:50 +0800

    Update 00-prepare_base.sh

--- openwrt/scripts/00-prepare_base.sh
+++ openwrt/scripts/00-prepare_base.sh
@@ -5,7 +5,7 @@
 sed -i 's/Os/O2/g' include/target.mk
 
 # 内核版本设置
-curl -s $mirror/tags/kernel-6.6 > include/kernel-6.6
+# curl -s $mirror/tags/kernel-6.6 > include/kernel-6.6
 curl -s $mirror/openwrt/patch/kernel-6.6/kernel/0001-linux-module-video.patch > package/0001-linux-module-video.patch
 git apply package/0001-linux-module-video.patch
 rm -rf package/0001-linux-module-video.patch
